2393466c0ef0adf645380330c357619f444a7fd3
Davi Aravechia <davi.aravechia@aluno.fmpsc.edu.br>
2025-03-10 11:10:05 -0300

    Add files via upload

---
+++ Aula4/atividade1.py
@@ -0,0 +1,5 @@
+valor1 = int(input("Insira um valor numerico"))
+valor2 = int(input("Insira um valor numerico"))
+
+print(valor1 == valor2)
+
---
+++ Aula4/exercicio5.py
@@ -0,0 +1,4 @@
+Celsius= float(input("Insira a temperatura em Celsius"))
+
+Fahrenheit = (Celsius -32)*5/9
+print(f"Sua temperatura em Fahrenheit é {Fahrenheit}°")
